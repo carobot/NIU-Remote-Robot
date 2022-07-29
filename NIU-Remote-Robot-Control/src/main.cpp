f13fc1284293d38c13f9938ea476d5ceee77ec55
AngryCircuit <contact@angrycircuit.com>
2022-07-29 16:05:38 -0400

    added motor and trigger control code

--- NIU-Remote-Robot-Control/src/main.cpp
+++ NIU-Remote-Robot-Control/src/main.cpp
@@ -1,6 +1,29 @@
 #include <Arduino.h>
 
 /*
+
+----------------------------
+UNO Pin Layout
+----------------------------
+
+					| D13 - Joystick SCK
+					| D12 - Joystick MISO
+					| D11 - Joystick MOSI
+					| D10 - Joystick SS
+					| D9* - Right Motor ENA
+					| D8  - Right Motor IN1
+					| D7  - Right Motor IN2
+					| D6* - Left Motor ENB
+				A0 || D5  - Left Motor IN4
+				A1 || D4  - Left Motor IN5
+				A2 || D3
+	Trigger -	A3 || D2
+				A4 || D1
+				A5 || D0
+
+Keep D2 and D3 open for interrupt
+Keep A4 and A5 open for I2C
+
 ----------------------------
 Joystick Receiver Connection
 ----------------------------
@@ -43,6 +66,8 @@ cu -l /dev/ttyACM0 -s 115200
 typedef const __FlashStringHelper * FlashStr;
 typedef const byte* PGM_BYTES_P;
 #define PSTR_TO_F(s) reinterpret_cast<const __FlashStringHelper *> (s)
+
+/// Joystick Stuff
 
 // This can be changed freely but please see above
 const byte PIN_PS2_ATT = 10;
@@ -160,7 +185,92 @@ const char* const controllerTypeStrings[PSCTRL_MAX + 1] PROGMEM = {
 	ctrlTypeOutOfBounds
 };
 
+/// Motor Stuff
 
+int left_speed = 0, right_speed = 0;
+
+//Motor 1
+const byte PIN_ENA = 9;
+const byte PIN_IN1 = 8;
+const byte PIN_IN2 = 7;
+
+// Motor B
+const byte PIN_ENB = 6;
+const byte PIN_IN3 = 5;
+const byte PIN_IN4 = 4;
+
+const byte PIN_TRIGGER = A3;
+
+void setLeftMotor(int speed) {
+
+	// TODO: Add in some kind of ramp
+	// if (speed <= left_speed) {
+	// 	// set motor speed to new speed if speed is lower, safety
+	// 	left_speed = speed;
+	// } else {
+	// 	// ramp up motor speed
+	// 	left_speed++;
+	// }
+	if (abs(speed) > 30) {
+		left_speed = speed;
+	} else {
+		left_speed = 0;
+	}
+
+	Serial.print (F("Left: "));
+
+	if (left_speed > 0) {
+		// Move forward
+		digitalWrite(PIN_IN1, HIGH);
+    	digitalWrite(PIN_IN2, LOW);
+		Serial.print (F("Forward: "));
+	} else if (left_speed < 0) {
+		// Move backward
+		digitalWrite(PIN_IN1, LOW);
+    	digitalWrite(PIN_IN2, HIGH);
+		Serial.print (F("Backward: "));
+	} else {
+		// Stop
+		digitalWrite(PIN_IN1, LOW);
+    	digitalWrite(PIN_IN2, LOW);
+		Serial.print (F("Stop: "));
+	}
+
+	analogWrite(PIN_ENA, abs(left_speed));
+	Serial.print (abs(left_speed));
+
+}
+
+void setRightMotor(int speed) {
+	//See above
+	if (abs(speed) > 30) {
+		right_speed = speed;
+	} else {
+		right_speed = 0;
+	}
+
+	Serial.print (F(" Right: "));
+
+	if (right_speed > 0) {
+		// Move forward
+		digitalWrite(PIN_IN3, HIGH);
+    	digitalWrite(PIN_IN4, LOW);
+		Serial.print (F("Forward: "));
+	} else if (right_speed < 0) {
+		// Move backward
+		digitalWrite(PIN_IN3, LOW);
+    	digitalWrite(PIN_IN4, HIGH);
+		Serial.print (F("Backward: "));
+	} else {
+		// Stop
+		digitalWrite(PIN_IN3, LOW);
+    	digitalWrite(PIN_IN4, LOW);
+		Serial.print (F("Stop: "));
+	}
+
+	analogWrite(PIN_ENB, abs(right_speed));
+	Serial.println (abs(right_speed));
+}
 
 /////////////////////////////////////////////////////////////
 //                      Main Code                          //
@@ -170,11 +280,20 @@ PsxControllerHwSpi<PIN_PS2_ATT> psx;
 
 boolean haveController = false;
 
-boolean debug_mode = true;
+boolean debug_mode = false;
 
 void setup () {
 	fastPinMode (PIN_BUTTONPRESS, OUTPUT);
 	fastPinMode (PIN_HAVECONTROLLER, OUTPUT);
+
+	fastPinMode (PIN_ENA, OUTPUT);
+	fastPinMode (PIN_IN1, OUTPUT);
+	fastPinMode (PIN_IN2, OUTPUT);
+	fastPinMode (PIN_ENB, OUTPUT);
+	fastPinMode (PIN_IN3, OUTPUT);
+	fastPinMode (PIN_IN4, OUTPUT);
+
+	fastPinMode (PIN_TRIGGER, OUTPUT);
 	
 	delay (300);
 
@@ -184,8 +303,12 @@ void setup () {
  
 void loop () {
 	static byte slx, sly, srx, sry;
+	static int left_speed_setpoint, right_speed_setpoint;
+	byte trigger_set = 0;
+
+	// Reading from Joystick start here VVV ////
 	
-  // DO NOT MODIFY FROM HERE to ... //
+	// DO NOT MODIFY FROM HERE to ... //
 	fastDigitalWrite (PIN_HAVECONTROLLER, haveController);
 	
 	if (!haveController) {
@@ -224,11 +347,11 @@ void loop () {
 		if (!psx.read ()) {
 			Serial.println (F("Controller lost :("));
 			haveController = false;
-  // ... DO NOT MODIFY END //
+	// ... DO NOT MODIFY END //
 
 		} else {
       
-      // HAVE Controller AND Input
+      	// HAVE Controller AND Input
 
 			fastDigitalWrite (PIN_BUTTONPRESS, !!psx.getButtonWord ());
 			if (debug_mode) dumpButtons (psx.getButtonWord ());
@@ -252,6 +375,28 @@ void loop () {
 		}
 	}
 
-	
+	// Reading from Joystick end here ^^^ ////
+
+	// Motor Control starts here VVV ////
+
+	left_speed_setpoint = map(sly, 0, 255, 255, -255);
+	right_speed_setpoint = map(sry, 0, 255, 255, -255);
+
+	setLeftMotor(left_speed_setpoint);
+	setRightMotor(right_speed_setpoint);
+
+	trigger_set = (psx.getButtonWord () & 0x2000) ? true : false;
+	//Serial.println (psx.getButtonWord ());
+
+	if (trigger_set) {
+		digitalWrite(PIN_TRIGGER, HIGH);
+		Serial.println (F("Trigger!"));
+	} else {
+		digitalWrite(PIN_TRIGGER, LOW);
+	}
+
+	// Motor Control end here ^^^ ////
+
+	// the delay might not be necessary if we have a lot of process
 	delay (1000 / 60);
 }
